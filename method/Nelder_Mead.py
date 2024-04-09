8f7a38725f1d73ffae428fcc8cb53d40dcc7bb9f
Arseniy <33318310+StatsenkoArs@users.noreply.github.com>
2024-04-09 14:29:56 +0300

    calculate works okay solve is broken btw

--- method/Nelder_Mead.py
+++ method/Nelder_Mead.py
@@ -3,13 +3,56 @@ import numpy as np
 
 class NelderMead:
 
-    def solve(self, f, n, s_point,alpha=1, beta=0.5, gamma=2):
+    def calculate(self, f, n, alpha=1.5, beta=1.5, gamma=1.5):
+        x = np.eye(n + 1, n)
+        for pepe in range(1000):
+            fun = [f(x0) for x0 in x]
+            fun_xr = min(fun)
+            xr, ind_xr = x[fun.index(fun_xr)], fun.index(fun_xr)
+            fun_xs = max(fun)
+            xs, ind_xs = x[fun.index(fun_xs)], fun.index(fun_xs)
+            x_mid = (sum(x) - xs) / n
+            x_sim = x_mid + alpha * (x_mid - xs)
+            fun_x_sim = f(x_sim)
+            if fun_xr > fun_x_sim:
+                x1 = x_mid + gamma * (x_sim - x_mid)
+                fun_x1 = f(x1)
+                if fun_x_sim > fun_x1:
+                    x[ind_xs] = x1
+                else:
+                    x[ind_xs] = x_sim
+            else:
+                fun_xss = fun_xr
+                xss = xr
+                ind_xss = ind_xr
+                for i in range(n + 1):
+                    if i != ind_xs and fun_xss < fun[i]:
+                        fun_xss = fun[i]
+                        xss = x[i]
+                        ind_xss = i
+                if fun_xss >= fun_x_sim:
+                    x[ind_xs] = x_sim
+                if fun_x_sim < fun_xs:
+                    fun_xl = fun_x_sim
+                    xl = xs
+                else:
+                    fun_xl = fun_xs
+                    xl = x_sim
+                xll = x_mid + beta * (xl - x_mid)
+                fun_xll = f(xll)
+                if fun_xll > fun_xl:
+                    x = x + 0.5 * (xr - x)
+                else:
+                    x[ind_xs] = xll
+        return x
+
+    def solve(self, f, n, s_point, alpha=1, beta=0.5, gamma=2):
 
         x0 = np.eye(n + 1, n)
         if not x0.__contains__(s_point):
             x0[n] = s_point
 
-        for pepe in range(1000):
+        for pepe in range(100):
             f0 = [f(x) for x in x0]
 
             if f0[0] > f0[1]:
@@ -35,7 +78,7 @@ class NelderMead:
                     f_xg = f0[i]
                     g = i
 
-            xc = np.array((sum(x0[:]) - x0[h]) / n)
+            xc = np.array((sum(x0[:]) - x0[h]) / n)  # убрать np.array можно
 
             xr = (1 + alpha) * xc - alpha * x0[h]
             f_xr = f(xr)
