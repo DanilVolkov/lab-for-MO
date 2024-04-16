055cc33b79a1f6a69d422af4b1e4e581e58ce321
StatsenkoArs <33318310+StatsenkoArs@users.noreply.github.com>
2024-04-16 18:36:03 +0300

    change names

--- method/nelder_mead.py
+++ method/nelder_mead.py
@@ -5,64 +5,64 @@ def symmetrical_point(x1, x2, a):
     return x1 + a * (x2 - x1)
 
 
-def average_except_one_point(arr, ind):
-    return (sum(arr) - arr[ind]) / (len(arr) - 1)
+def average_except_one_point(array, exception_index):
+    return (sum(array) - array[exception_index]) / (len(array) - 1)
 
 
-def index_second_max(arr):
-    sec_max = min(arr)
-    m = arr.index(max(arr))
-    l_arr = len(arr)
-    for i in range(l_arr):
-        if i != m and sec_max < arr[i]:
-            sec_max = arr[i]
-    return arr.index(sec_max)
+def index_second_max(array):
+    second_max = min(array)
+    index_max = array.index(max(array))
+    len_array = len(array)
+    for i in range(len_array):
+        if i != index_max and second_max < array[i]:
+            second_max = array[i]
+    return array.index(second_max)
 
 
-def index_min_max(arr):
-    return arr.index(min(arr)), arr.index(max(arr))
+def index_min_max(array):
+    return array.index(min(array)), array.index(max(array))
 
 
-def shrink_points(arr_points, arr_values):
-    ind_min = arr_values.index(min(arr_values))
-    return symmetrical_point(arr_points, arr_points[ind_min], 0.5)
+def shrink_points(array_points, array_values):
+    index_min = array_values.index(min(array_values))
+    return symmetrical_point(array_points, array_points[index_min], 0.5)
 
 
-def calculate_neldermead(fun, dim, alpha=1.5, beta=1.5, gamma=1.5):
-    if alpha <= 0 or beta <= 1 or gamma <= 1 or dim <= 0:
+def calculate_neldermead(function, dimension, alpha=1.5, beta=1.5, gamma=1.5):
+    if alpha <= 0 or beta <= 1 or gamma <= 1 or dimension <= 0:
         raise RuntimeError
-    simplex = np.eye(dim + 1, dim)
+    simplex = np.eye(dimension + 1, dimension)
     while not np.isclose(simplex, simplex[0]).all():
 
-        fun_val = [fun(x0) for x0 in simplex]
+        function_value = [function(x0) for x0 in simplex]
 
-        ind_min, ind_max = index_min_max(fun_val)
+        index_min, index_max = index_min_max(function_value)
 
-        avg_x = average_except_one_point(simplex, ind_max)
-        sym_x = symmetrical_point(avg_x, simplex[ind_max], -alpha)
+        average_x = average_except_one_point(simplex, index_max)
+        symmetrical_x = symmetrical_point(average_x, simplex[index_max], -alpha)
 
-        if fun_val[ind_min] > fun(sym_x):
-            x1 = symmetrical_point(avg_x, sym_x, gamma)
-            if fun(sym_x) > fun(x1):
-                simplex[ind_max] = x1
+        if function_value[index_min] > function(symmetrical_x):
+            x1 = symmetrical_point(average_x, symmetrical_x, gamma)
+            if function(symmetrical_x) > function(x1):
+                simplex[index_max] = x1
             else:
-                simplex[ind_max] = sym_x
+                simplex[index_max] = symmetrical_x
 
         else:
-            if fun_val[index_second_max(fun_val)] >= fun(sym_x):
-                simplex[ind_max] = sym_x
+            if function_value[index_second_max(function_value)] >= function(symmetrical_x):
+                simplex[index_max] = symmetrical_x
 
-            if fun(sym_x) < fun_val[ind_max]:
-                fun_x2 = fun(sym_x)
-                x2 = sym_x
+            if function(symmetrical_x) < function_value[index_max]:
+                function_x2 = function(symmetrical_x)
+                x2 = symmetrical_x
             else:
-                fun_x2 = fun_val[ind_max]
-                x2 = simplex[ind_max]
+                function_x2 = function_value[index_max]
+                x2 = simplex[index_max]
 
-            x3 = symmetrical_point(avg_x, x2, beta)
-            if fun(x3) > fun_x2:
-                simplex = shrink_points(simplex, fun_val)
+            x3 = symmetrical_point(average_x, x2, beta)
+            if function(x3) > function_x2:
+                simplex = shrink_points(simplex, function_value)
             else:
-                simplex[ind_max] = x3
+                simplex[index_max] = x3
 
-    return np.round(sum(simplex) / (dim + 1), 5)
+    return np.round(sum(simplex) / (dimension + 1), 5)
