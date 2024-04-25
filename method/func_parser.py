56f7e7ff723ca44d53f36e0198cb5cbc09da1169
ActiWait <trosio2003@gmail.com>
2024-04-25 15:46:06 +0300

    rename variables in func_parser.py

--- method/func_parser.py
+++ method/func_parser.py
@@ -1,41 +1,41 @@
 import re
 
 
-def add_math(func_str: str) -> str:
+def add_math(func: str) -> str:
     patterns = ["sqrt", "exp", "sin", "cos", "tan", "log"]
     for pattern in patterns:
-        func_str = re.sub(pattern, "math." + pattern, func_str)
-    return func_str
+        func = re.sub(pattern, "math." + pattern, func)
+    return func
 
 
-def check_for_correct_func(func_str: str) -> None:
+def check_for_correct_func(func: str) -> None:
     allowed_char_pattern = r"[^acegilnopqrstx\^0-9\(\)\+\-/\*,\. ]"
-    if len(re.findall(allowed_char_pattern, func_str)) != 0:
+    if len(re.findall(allowed_char_pattern, func)) != 0:
         raise Exception("Строка некорректна")
 
 
-def add_brackets_and_indexes(func_str: str) -> str:
+def add_brackets_and_indexes(func: str) -> str:
     pattern_for_x = r"x\d+"
-    x_group = re.findall(pattern_for_x, func_str)
+    x_group = re.findall(pattern_for_x, func)
     for i in range(len(x_group)):
         numb = "x[" + str(int(x_group[i][1]) - 1) + "]"
-        func_str = re.sub(pattern_for_x, numb, func_str, 1)
-    return func_str
+        func = re.sub(pattern_for_x, numb, func, 1)
+    return func
 
 
-def repl_pow_sym(func_str: str) -> str:
-    return re.sub(r"\^", "**", func_str)
+def repl_pow_sym(func: str) -> str:
+    return re.sub(r"\^", "**", func)
 
 
-def count_variables(func_str: str) -> int:
+def count_variables(func: str) -> int:
     pattern_for_var = r"(?<=\[)\d+(?=\])"
-    return len(set(re.findall(pattern_for_var, func_str)))
+    return len(set(re.findall(pattern_for_var, func)))
 
 
-def function_parsing(func_str: str) -> tuple[int, str]:
-    check_for_correct_func(func_str)
-    func_str = add_brackets_and_indexes(func_str)
-    func_str = repl_pow_sym(func_str)
-    func_str = add_math(func_str)
-    dimensions = count_variables(func_str)
-    return dimensions, func_str
+def function_parsing(func: str) -> tuple[int, str]:
+    check_for_correct_func(func)
+    func = add_brackets_and_indexes(func)
+    func_result = repl_pow_sym(func)
+    func_result = add_math(func_result)
+    dimensions = count_variables(func_result)
+    return dimensions, func
