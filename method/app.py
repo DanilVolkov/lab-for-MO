119e5b771d16a446f75e42c5d60e4b5ce264d297
Arseniy <33318310+StatsenkoArs@users.noreply.github.com>
2024-05-04 01:09:57 +0300

    should work

--- method/app.py
+++ method/app.py
@@ -1,14 +1,18 @@
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtCore
 from main_design import Ui_MainWindow
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import numpy as np
 from func_parser import function_parsing
 from nelder_mead import calculate_neldermead
+import nelder_mead as nm
 import sys
         
 
 class mywindow(QtWidgets.QMainWindow):
+    
+    max_iterations = 1000
+    delay = 2000
     
     function = lambda self, x: eval("x[0]**2 + x[1]**2")
     
@@ -18,6 +22,12 @@ class mywindow(QtWidgets.QMainWindow):
     right_x2 = 10
     
     dimensions = 0
+    
+    timer = QtCore.QTimer()
+    
+    simplex = np.eye(dimensions + 1, dimensions)
+    
+    iteration = 0
     
     def __init__(self):
         super(mywindow, self).__init__()
@@ -33,12 +43,19 @@ class mywindow(QtWidgets.QMainWindow):
         
         self.refresh_plot()
         
+        self.timer.timeout.connect(self.make_step)
+        
         self.ui.lineEdit.textEdited.connect(self.function_changed)
+        self.ui.lineEdit.setText("x1**2 + x2**2")
         
         self.ui.leftx1.textEdited.connect(self.interval_changed)
         self.ui.leftx2.textEdited.connect(self.interval_changed)
         self.ui.rightx1.textEdited.connect(self.interval_changed)
         self.ui.rightx2.textEdited.connect(self.interval_changed)
+        self.ui.leftx1.setText(str(self.left_x1))
+        self.ui.leftx2.setText(str(self.left_x2))
+        self.ui.rightx1.setText(str(self.right_x1))
+        self.ui.rightx2.setText(str(self.right_x2))
         
         self.ui.pushButton.clicked.connect(self.btnClicked)
         self.ui.pushButton.setEnabled(False)
@@ -57,14 +74,47 @@ class mywindow(QtWidgets.QMainWindow):
             
     
     def btnClicked(self):
-        points = calculate_neldermead(self.function, self.dimensions)
-        output = ""
-        count = 1
-        for point in points:
-            output += "x" + str(count) + " = " + str(np.round(point, 2)) + "\n"
-            count += 1
-        output += "Q(X) = " + str(np.round(self.function(points), 4))
-        self.ui.label.setText(output)
+        points = np.zeros(1)
+        if self.dimensions == 2:
+            self.simplex = np.eye(self.dimensions + 1, self.dimensions)
+            self.iteration = 0
+            self.draw_simplex()
+            self.make_step()
+        else:
+            points = calculate_neldermead(self.function, self.dimensions)
+            self.show_results(points)
+        
+        
+    def make_step(self):
+        if (self.iteration > self.max_iterations or
+                np.allclose(self.simplex, self.simplex[0])):
+            self.timer.stop()
+            points = np.round(sum(self.simplex) / (self.dimensions + 1), 5)
+            self.show_results(points)
+        self.simplex = nm.make_step(self.function, self.simplex)
+        self.draw_simplex()
+        self.timer.start(self.delay)
+        
+    def draw_simplex(self):
+        self.refresh_plot()
+        z = np.zeros((self.dimensions + 1))
+        i = 0
+        for point in self.simplex:
+            z[i] = self.function(point)
+            i += 1
+        self.ui.widget.axes.scatter(self.simplex[:, 0], 
+                                    self.simplex[:, 1], 
+                                    z, edgecolors="red",
+                                    linewidth=4)
+        for i in range(self.dimensions + 1):
+            for j in range(i, self.dimensions + 1):
+                self.ui.widget.axes.plot([self.simplex[i, 0],self.simplex[j, 0]], 
+                                        [self.simplex[i, 1],self.simplex[j, 1]], 
+                                        [z[i], z[j]],
+                                        color="r")
+        self.ui.widget.canvas.draw()
+        self.ui.widget.show()
+        
         
     def interval_changed(self):
         if self.ui.leftx1.text().isdigit():
@@ -81,6 +131,16 @@ class mywindow(QtWidgets.QMainWindow):
              
         self.refresh_plot()
         
+    def show_results(self, points):
+        output = ""
+        count = 1
+        for point in points:
+            output += "x" + str(count) + " = " + str(np.round(point, 2)) + "\n"
+            count += 1
+        output += "Q(X) = " + str(np.round(self.function(points), 4))
+        self.ui.label.setText(output)
+        
+        
     def refresh_plot(self):
         for ax in self.ui.widget.fig.axes:
             ax.clear()
@@ -92,7 +152,7 @@ class mywindow(QtWidgets.QMainWindow):
         X[0], X[1] = np.meshgrid(X[0], X[1])
         Z = self.function(X)
 
-        self.ui.widget.axes.plot_surface(X[0], X[1], Z)
+        self.ui.widget.axes.plot_wireframe(X[0], X[1], Z)
         self.ui.widget.canvas.draw()
         self.ui.widget.show()
         
