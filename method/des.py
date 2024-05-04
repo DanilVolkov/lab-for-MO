4058478e06124ea79ba5f22074b46665346d9de9
Arseniy <33318310+StatsenkoArs@users.noreply.github.com>
2024-05-04 21:40:52 +0300

    refactor autogen file with layouts

--- method/des.py
+++ method/des.py
@@ -1,23 +1,7 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'maindSdIvV.ui'
-##
-## Created by: Qt User Interface Compiler version 6.7.0
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
-from PyQt5.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PyQt5.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
-    QLayout, QLineEdit, QMainWindow, QPushButton,
-    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
+from PyQt5.QtCore import (QCoreApplication, QMetaObject, QSize, Qt)
+from PyQt5.QtWidgets import (QGridLayout, QHBoxLayout, QLabel,
+                             QLayout, QLineEdit, QPushButton,
+                             QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
