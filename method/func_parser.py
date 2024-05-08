410cf5763044a100bfb42258714f6e3107a6e111
ActiWait <trosio2003@gmail.com>
2024-05-08 17:30:08 +0300

    replacing math with numpy

--- method/func_parser.py
+++ method/func_parser.py
@@ -1,15 +1,16 @@
 import re
 
 
-def add_math(func_str):
-    patterns = ["sqrt", "exp", "sin", "cos", "tan", "log"]
+def add_np(func_str):
+    patterns = ["sqrt", "exp", "sin", "cos", "tan", "pi"]
     for pattern in patterns:
-        func_str = re.sub(pattern, "math." + pattern, func_str)
+        func_str = re.sub(pattern, "np." + pattern, func_str)
+    func_str = replace_log(func_str)
     return func_str
 
 
 def check_for_correct_func(func_str):
-    allowed_char_pattern = r"[^acegilnopqrstx\^0-9\(\)\+\-/\*,\. ]"
+    allowed_char_pattern = r"[^acegilnopqrstx\^0-9\(\)\+\-\/\*,\. ]"
     if len(re.findall(allowed_char_pattern, func_str)) != 0:
         raise Exception("Строка некорректна")
 
@@ -32,10 +33,16 @@ def count_variables(func_str):
     return len(set(re.findall(pattern_for_var, func_str)))
 
 
+def replace_log(func_str):
+    func_str = re.sub("ln", "np.log", func_str)
+    func_str = re.sub("log", "np.emath.logn", func_str)
+    return func_str
+
+
 def function_parsing(func_str):
     check_for_correct_func(func_str)
     func_str = add_brackets_and_indexes(func_str)
     func_str = repl_pow_sym(func_str)
-    func_str = add_math(func_str)
+    func_str = add_np(func_str)
     dimensions = count_variables(func_str)
     return dimensions, func_str
